1f6e66a2c073b39a84d8b2358f0872272ec14ab7
Tacoder9 <chris.pylinski@googlemail.com>
2016-06-08 21:22:03 +0200

    Create .sharpMatlab.sh

---
+++ .sharpMatlab.sh
@@ -0,0 +1,12 @@
+!/bin/sh
+# Script to change path of Java Plugins in order to prevent blurry appearance
+# ./start_Matlab.sh
+# alias somescript.sh='. ./somescript.sh'
+clear
+
+echo "The script for MATLAB R2014a starts now..."
+echo "export..."
+export MATLAB_JAVA=/Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Home
+cd /Applications/MATLAB_R2014a.app/bin
+./matlab -nosplash
+
